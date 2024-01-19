8a8d45d4e48134426e0d2491ee786ed6bb4714c5
Arnab <arnabing@Arnabs-MacBook-Pro.local>
2024-01-18 19:48:05 -0500

    form now resets so multiple cards can be processed

--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Bind the form submission to a function
-    document.querySelector('form').onsubmit = function(event) {
-        // Prevent the default form submission
+    const form = document.querySelector('form');
+    const fileInput = document.querySelector('input[type="file"]');
+
+    form.onsubmit = function(event) {
         event.preventDefault();
 
-        // Get the file from the form input
-        let fileInput = document.querySelector('input[type="file"]');
         let formData = new FormData();
         formData.append('file', fileInput.files[0]);
 
-        // Make an AJAX call to the server with the file
         fetch('/upload', {
             method: 'POST',
             body: formData
@@ -18,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             console.log(data); // Log the data
             displayHtmlTable(data.html_table); // Display the HTML table
+            resetForm(); // Reset the form after submission
         })
         .catch(error => {
             console.error('Error:', error);
@@ -25,19 +24,19 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 });
 
-// Function to display the HTML table
+function resetForm() {
+    document.querySelector('form').reset();
+}
+
 function displayHtmlTable(htmlTable) {
     const tableContainer = document.getElementById('table-container');
-    // Check if the htmlTable is not null or undefined
     if (htmlTable) {
-        tableContainer.innerHTML = htmlTable; // Insert the HTML table
-        // Find the table within the container and add Bootstrap classes for styling
+        tableContainer.innerHTML = htmlTable; 
         const table = tableContainer.querySelector('table');
         if (table) {
-            table.classList.add('table', 'table-bordered'); // Apply Bootstrap table styles
+            table.classList.add('table', 'table-bordered'); 
         }
     } else {
-        // If htmlTable is null or undefined, show an error message or a default state
         tableContainer.innerHTML = '<p>No table data available.</p>';
     }
 }
